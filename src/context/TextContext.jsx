04227d164f701b268a9551076cbddc390b4d3e82
Omowunmikamil <balikiskamil@gmail.com>
2025-02-21 11:54:01 +0000

    refactor chat functionality and enhance bot responses with action buttons

--- src/context/TextContext.jsx
+++ src/context/TextContext.jsx
@@ -15,35 +15,55 @@ const TextProvider = ({ children }) => {
           },
         ];
   });
-
   const [input, setInput] = useState("");
-
-  useEffect(() => {
-    localStorage.setItem("chatMessages", JSON.stringify(messages));
-  }, [messages]);
 
   const sendMessage = () => {
     if (input.trim() === "") return;
-    const newMessages = [...messages, { text: input, sender: "user" }];
-    setMessages(newMessages);
+    setMessages((prev) => {
+      const newMessages = [...prev, { text: input, sender: "user" }];
+      localStorage.setItem("chatMessages", JSON.stringify(newMessages));
+      return newMessages;
+    });
     setInput("");
 
+    // Simulate bot response after 1 second
     setTimeout(() => {
-      const botReply = [
-        ...newMessages,
-        { text: "I got your message!", sender: "bot" },
-      ];
-      setMessages(botReply);
+      setMessages((prev) => {
+        const newMessages = [
+          ...prev,
+          {
+            text: "Choose your preferred language!",
+
+            buttons: [
+              {
+                text: "Translate",
+                action: "translate",
+                style:
+                  "bg-gray-300 text-gray-700 px-4 py-2 rounded-md shadow-md hover:bg-gray-600 hover:text-whit",
+              },
+              {
+                text: "Summarize",
+                action: "summarize",
+                style:
+                  "bg-gray-300 text-gray-700 px-4 py-2 rounded-md shadow-md hover:bg-gray-600 hover:text-whit",
+              },
+            ],
+            sender: "bot",
+          },
+        ];
+        localStorage.setItem("chatMessages", JSON.stringify(newMessages));
+        return newMessages;
+      });
     }, 1000);
   };
 
   const startNewChat = () => {
     const defaultMessages = [
-      { text: "Hello! 👋", sender: "bot" },
-      { text: "I am here to help you with your translations!", sender: "bot" },
-    ];
-    setMessages(defaultMessages);
-    localStorage.setItem("chatMessages", JSON.stringify(defaultMessages));
+        { text: "Hello! 👋", sender: "bot" },
+        { text: "I am here to help you with your translations!", sender: "bot" },
+      ];
+      setMessages(defaultMessages);
+      localStorage.removeItem("chatMessages");
   };
 
   return (
